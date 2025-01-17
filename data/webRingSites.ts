49c01ff51fb7112da3346a7a9410d5564700f690
Elliot <elliotsaaen@hotmail.com>
2025-01-17 13:37:37 +0100

    Add new web ring site for recipes

--- data/webRingSites.ts
+++ data/webRingSites.ts
@@ -4,4 +4,9 @@ export const webRingSites = [
     url: "https://portfolio.aaenz.com",
     description: "My portfolio, made to look like and work like Windows XP!",
   },
+  {
+    name: "My Recipes",
+    url: "https://recipes.aaenz.com",
+    description: "Input your ingredients, utilities, etc, and get a recipe!",
+  },
 ];
